4991eccdc56ca76bc34734ccfb9a2fae3f462aed
hyeongyu36 <hyeongyu36@gmail.com>
2023-09-14 19:00:28 +0900

    feat : 팬덤랭킹 애니메이션추가

--- js/fandom.js
+++ js/fandom.js
@@ -1,6 +1,16 @@
 //  dom (html 태그 로딩 완료시 실행)
 $(document).ready(function () {
-
+    var swiper = new Swiper('#fandombox', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        
+        // autoHeight: true,
+        pagination: {
+          el: '.rank',
+          clickable: true,
+        }
+      });
 });
 
 // 멀티미디어 리소스 로딩 완료 후 실행
