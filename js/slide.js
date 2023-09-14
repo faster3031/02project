6ab74c00b4889ff4d1520b0857d1564af936a997
bejunhee <nowaffle1753@gmail.com>
2023-09-14 18:21:31 +0900

    캠페인 작업 진행중 및 이미지 변경 및 추가

--- js/slide.js
+++ js/slide.js
@@ -3,7 +3,7 @@ $(document).ready(function () {});
 
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function () {
-  var swiper = new Swiper(".mySwiper", {
+  var swiper = new Swiper(".slide_Swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
