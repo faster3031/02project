403cc26df3d95d339ab286327f8229a39d50969b
hyeongyu36 <hyeongyu36@gmail.com>
2023-09-15 18:15:05 +0900

    feat : fandom파트 수정, goods파트 수정
    fandom js 수정, 돌아가는 행성 위치수정

--- js/fandom.js
+++ js/fandom.js
@@ -1,16 +1,20 @@
 //  dom (html 태그 로딩 완료시 실행)
 $(document).ready(function () {
-    var swiper = new Swiper('#fandombox', {
+    var swiper = new Swiper("#fandombox", {
         spaceBetween: 30,
-        effect: 'fade',
+      effect: 'fade',
+      autoplay: {     //자동슬라이드 (false-비활성화)
+        delay: 5000, // 시간 설정
+        disableOnInteraction: false, // false-스와이프 후 자동 재생
+      },
+
         loop: true,
-        
-        // autoHeight: true,
         pagination: {
-          el: '.rank',
+          el: ".swiper-pagination",
           clickable: true,
-        }
-      });
+        },
+    });
+
 });
 
 // 멀티미디어 리소스 로딩 완료 후 실행
