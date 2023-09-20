22f6c3ab9b875c4675bf00092c4c1522d544c13f
hyeongyu36 <hyeongyu36@gmail.com>
2023-09-20 09:48:17 +0900

    feat : review페이지 추가, index 순서 재배치
    review-html,css,js 추가 및 수정 / index 순서 재배치, font.css 추가,goods css수정

--- js/fandom.js
+++ js/fandom.js
@@ -1,23 +1,20 @@
 //  dom (html 태그 로딩 완료시 실행)
 $(document).ready(function () {
-    var swiper = new Swiper("#fandombox", {
-        spaceBetween: 30,
-      effect: 'fade',
-      // autoplay: {     //자동슬라이드 (false-비활성화)
-      //   delay: 5000, // 시간 설정
-      //   disableOnInteraction: false, // false-스와이프 후 자동 재생
-      // },
+  var swiper = new Swiper("#fandombox", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {     //자동슬라이드 (false-비활성화)
+      delay: 5000, // 시간 설정
+      disableOnInteraction: false, // false-스와이프 후 자동 재생
+    },
 
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-    });
-
+    loop: true,
+    pagination: {
+      el: ".fandom .swiper-pagination",
+      clickable: true,
+    },
+  });
 });
 
 // 멀티미디어 리소스 로딩 완료 후 실행
-window.addEventListener("load", function () {
-
-});
+window.addEventListener("load", function () {});
---
+++ js/review.js
@@ -0,0 +1,16 @@
+//  dom (html 태그 로딩 완료시 실행)
+$(document).ready(function () {});
+
+// 멀티미디어 리소스 로딩 완료 후 실행
+window.addEventListener("load", function () {
+  // $('#pagination-demo').twbsPagination({
+  //     totalPages: 16,
+  //     visiblePages: 6,
+  //     next: 'Next',
+  //     prev: 'Prev',
+  //     onPageClick: function (event, page) {
+  //         //fetch content and render here
+  //         $('#board_st').text('Page ' + page) + ' content here';
+  //     }
+  // });
+});
