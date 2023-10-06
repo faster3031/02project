d36f792fd39590ea4266ccae1316c948ed28c3e9
faster3031 <dongoh3031@gmail.com>
2023-10-06 15:39:49 +0900

    feat : artist 수정

--- js/artist.js
+++ js/artist.js
@@ -20,24 +20,60 @@ const toggleButton = document.getElementById('toggleBtn');
 const section = document.getElementById('artist-hidden');
 
 // 버튼 클릭 이벤트 핸들러 추가
-toggleButton.addEventListener('click', function () {
-    // 섹션의 현재 표시 상태를 확인하고 토글하기
-    if (section.style.display === 'none' || section.style.display === '') {
-        // 섹션이 숨겨져 있으면 나타내기
-        section.style.display = 'block';
-    } else {
-        // 섹션이 보이고 있으면 감추기
-        section.style.display = 'none';
-    }
-});
 $(".custom-carousel").owlCarousel({
     autoWidth: true,
-    loop: true,
   });
   $(document).ready(function () {
     $(".custom-carousel .item").click(function () {
       $(".custom-carousel .item").not($(this)).removeClass("active");
       $(this).toggleClass("active");
     });
+        // 검색 이벤트 핸들러
+        $("#searchBtn").click(function () {
+            var searchTerm = $("#searchInput").val().toLowerCase(); // 검색어를 소문자로 변환
+    
+            // 모든 슬라이드에서 "active" 클래스 제거
+            $(".custom-carousel .item").removeClass("active");
+    
+            // 슬라이드를 검색어와 비교하여 일치하는 경우 "active" 클래스 추가
+            $(".custom-carousel .item").each(function () {
+                var slideTitle = $(this).find(".item-desc h3").text().toLowerCase(); // 제목 텍스트 추출
+                var slideContent = $(this).find(".item-desc p").text().toLowerCase(); // 내용 텍스트 추출
+                
+                // 제목 또는 내용 중에서 검색어와 일치하는 경우 "active" 클래스 추가
+                if (slideTitle.includes(searchTerm) || slideContent.includes(searchTerm)) {
+                    $(this).addClass("active");
+                }
+            });
+        });
+        var owl = $(".custom-carousel").owlCarousel({
+          autoWidth: true
+      });
+  
+      // 검색 이벤트 핸들러
+      $("#searchBtn").click(function () {
+          var searchTerm = $("#searchInput").val().toLowerCase(); // 검색어를 소문자로 변환
+  
+          // 모든 슬라이드에서 "active" 클래스 제거
+          $(".custom-carousel .item").removeClass("active");
+  
+          // 슬라이드를 검색어와 비교하여 일치하는 경우 "active" 클래스 추가
+          $(".custom-carousel .item").each(function () {
+              var slideTitle = $(this).find(".item-desc h3").text().toLowerCase(); // 제목 텍스트 추출
+              var slideContent = $(this).find(".item-desc p").text().toLowerCase(); // 내용 텍스트 추출
+              
+              // 제목 또는 내용 중에서 검색어와 일치하는 경우 "active" 클래스 추가
+              if (slideTitle.includes(searchTerm) || slideContent.includes(searchTerm)) {
+                  $(this).addClass("active");
+              }
+          });
+  
+          // "active" 클래스가 추가된 슬라이드의 인덱스 찾기
+          var activeSlideIndex = $(".custom-carousel .item.active").index();
+  
+          // 중앙 슬라이드로 이동
+          owl.trigger('to.owl.carousel', [activeSlideIndex, 0, true]);
+          
+      });
   });
 });
