c330cde998d68a6328f345d07637f922d1ed5a10
faster3031 <dongoh3031@gmail.com>
2023-10-06 17:49:33 +0900

    feat : artist 섹션 완료 header 메뉴 스크롤 이벤트 적용

--- js/artist.js
+++ js/artist.js
@@ -5,49 +5,16 @@ $(document).ready(function () {
 
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.addEventListener("load", function () {
-  
-    var divs = document.querySelectorAll(".card-list div");
-divs.forEach(function (elm) {
-  elm.addEventListener("mouseenter", function () {
-    var panel = document.querySelector(".highlight");
-    panel.classList.toggle("highlight");
-    elm.classList.toggle("highlight");
-  });
-});
-window.focus();
-// 버튼과 섹션 요소를 가져오기
-const toggleButton = document.getElementById('toggleBtn');
-const section = document.getElementById('artist-hidden');
-
 // 버튼 클릭 이벤트 핸들러 추가
 $(".custom-carousel").owlCarousel({
-    autoWidth: true,
+  autoWidth: true,
   });
-  $(document).ready(function () {
     $(".custom-carousel .item").click(function () {
       $(".custom-carousel .item").not($(this)).removeClass("active");
       $(this).toggleClass("active");
     });
-        // 검색 이벤트 핸들러
-        $("#searchBtn").click(function () {
-            var searchTerm = $("#searchInput").val().toLowerCase(); // 검색어를 소문자로 변환
-    
-            // 모든 슬라이드에서 "active" 클래스 제거
-            $(".custom-carousel .item").removeClass("active");
-    
-            // 슬라이드를 검색어와 비교하여 일치하는 경우 "active" 클래스 추가
-            $(".custom-carousel .item").each(function () {
-                var slideTitle = $(this).find(".item-desc h3").text().toLowerCase(); // 제목 텍스트 추출
-                var slideContent = $(this).find(".item-desc p").text().toLowerCase(); // 내용 텍스트 추출
-                
-                // 제목 또는 내용 중에서 검색어와 일치하는 경우 "active" 클래스 추가
-                if (slideTitle.includes(searchTerm) || slideContent.includes(searchTerm)) {
-                    $(this).addClass("active");
-                }
-            });
-        });
         var owl = $(".custom-carousel").owlCarousel({
-          autoWidth: true
+          autoWidth: true,
       });
   
       // 검색 이벤트 핸들러
@@ -60,7 +27,7 @@ $(".custom-carousel").owlCarousel({
           // 슬라이드를 검색어와 비교하여 일치하는 경우 "active" 클래스 추가
           $(".custom-carousel .item").each(function () {
               var slideTitle = $(this).find(".item-desc h3").text().toLowerCase(); // 제목 텍스트 추출
-              var slideContent = $(this).find(".item-desc p").text().toLowerCase(); // 내용 텍스트 추출
+              var slideContent = $(this).find(".item-desc h4").text().toLowerCase(); // 내용 텍스트 추출
               
               // 제목 또는 내용 중에서 검색어와 일치하는 경우 "active" 클래스 추가
               if (slideTitle.includes(searchTerm) || slideContent.includes(searchTerm)) {
@@ -71,9 +38,8 @@ $(".custom-carousel").owlCarousel({
           // "active" 클래스가 추가된 슬라이드의 인덱스 찾기
           var activeSlideIndex = $(".custom-carousel .item.active").index();
   
-          // 중앙 슬라이드로 이동
-          owl.trigger('to.owl.carousel', [activeSlideIndex, 0, true]);
+          // 특정 슬라이드 위치로 이동
+          owl.trigger('to.owl.carousel', [activeSlideIndex, 200, true]);
           
       });
   });
-});
--- js/header.js
+++ js/header.js
@@ -55,16 +55,50 @@ window.addEventListener("load", function () {
     }
   });
 
-  window.addEventListener("scroll", function () {
-    scy = window.document.documentElement.scrollTop;
-    // console.log(scy);
-    if (scy > scActive) {
-      header.classList.add("header-active");
-    } else {
-      header.classList.remove("header-active");
-    }
+  // 각 섹션의 위치값(세로스크롤 위치)
+  const sectionYpos = [0, 2700, 3500,0];
+  // 클래스 nav 의 li 를 찾아라
+  // 저장한다. 재활용하기 위해서
+  const navLis = $(".nav ul li");
+  // li 에 a 태그를 클릭을 해서 스크롤을 이동
+  const navLisA = $(".nav ul li a");
+  // 클릭 기능 구현
+  $.each(navLisA, function (index, item) {
+    // item 은 a 태그를 말합니다.
+    // item 을 클릭을 할 겁니다.
+    // item 은 html 태그 (jQuery 용도)
+    $(this).click(function (event) {
+      // li 의 모든 클래스 제거
+      navLis.removeClass("focus-active");
+      // 클릭된 li 에 focus-active 추가하기
+      navLis.eq(index).addClass("focus-active");
+      // 2. 클릭하면 스크롤바가 움직인다.
+      $("html, body").animate({ scrollTop: sectionYpos[index] }, "slow");
+    });
   });
 
+  //  반응형
+    // 각 섹션의 위치값(세로스크롤 위치)
+    const mbsectionYpos = [0, 2700, 3500,0];
+    // 클래스 nav 의 li 를 찾아라
+    // 저장한다. 재활용하기 위해서
+    const mbnavLis = $(".nav-mb ul li");
+    // li 에 a 태그를 클릭을 해서 스크롤을 이동
+    const mbnavLisA = $(".nav-mb ul li a");
+    // 클릭 기능 구현
+    $.each(mbnavLisA, function (index, item) {
+      // item 은 a 태그를 말합니다.
+      // item 을 클릭을 할 겁니다.
+      // item 은 html 태그 (jQuery 용도)
+      $(this).click(function (event) {
+        // li 의 모든 클래스 제거
+        mbnavLis.removeClass("focus-active");
+        // 클릭된 li 에 focus-active 추가하기
+        mbnavLis.eq(index).addClass("focus-active");
+        // 2. 클릭하면 스크롤바가 움직인다.
+        $("html, body").animate({ scrollTop: mbsectionYpos[index] }, "slow");
+      });
+    });
   // -------------------------------------------------------로그인
   // 페이지가 로드될 때 초기 대시보드 화면을 표시
 
